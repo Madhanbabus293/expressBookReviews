a9d1b1125b26bc0c6d70419dd566255b2b02b74e
madhanbabu <madhanbabu293@gmail.com>
2024-11-09 15:25:10 +0530

    update async/await

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -14,20 +14,40 @@ public_users.post("/register", (req, res) => {
   }
 });
 
+const getBooks = (details) => {
+  if (!details) {
+    return books;
+  }
+  const { isbn, author = '', title = '' } = details;
+
+  if (isbn) {
+    return books[isbn];
+  }
+
+  console.log(details)
+  return Object.fromEntries(
+    Object.entries(books).filter(
+      ([id, book]) => book.author === author || book.title === title
+    )
+  );
+};
+
 // Get the book list available in the shop
-public_users.get("/", function async(req, res) {
+public_users.get("/", async function(req, res) {
   //Write your code here
   try {
-    return res.status(200).json({ books: JSON.stringify(books) });
+    const bookDetails = await getBooks();
+    console.log(bookDetails)
+    return res.status(200).json({ books: JSON.stringify(bookDetails) });
   } catch (err) {
     return res.status(400).json({ mesaage: "issue in retrive books" });
   }
 });
 
 // Get book details based on ISBN
-public_users.get("/isbn/:isbn", function (req, res) {
+public_users.get("/isbn/:isbn", async function(req, res) {
   try {
-    const filteredBooks = books[req.params.isbn] || {};
+    const filteredBooks = await getBooks({ isbn: req.params.isbn });
     return res.status(200).json({ books: JSON.stringify(filteredBooks) });
   } catch (err) {
     return res.status(400).json({ mesaage: "issue in filter by isbn" });
@@ -35,38 +55,34 @@ public_users.get("/isbn/:isbn", function (req, res) {
 });
 
 // Get book details based on author
-public_users.get("/author/:author", function (req, res) {
+public_users.get("/author/:author", async function (req, res) {
   try {
-    const filteredBooks = Object.entries(books).filter(
-      ([id, book]) => book.author === req.params.author
-    );
+    const filteredBooks = await getBooks({ author: req.params.author })
     return res
       .status(200)
-      .json({ books: JSON.stringify(Object.fromEntries(filteredBooks)) });
+      .json({ books: JSON.stringify(filteredBooks) });
   } catch (err) {
     return res.status(400).json({ mesaage: "issue in filter by author" });
   }
 });
 
 // Get all books based on title
-public_users.get("/title/:title", function (req, res) {
+public_users.get("/title/:title", async function (req, res) {
   try {
-    const filteredBooks = Object.entries(books).filter(
-      ([id, book]) => book.title === req.params.title
-    );
+    const filteredBooks = await getBooks({ title: req.params.title })
     return res
       .status(200)
-      .json({ books: JSON.stringify(Object.fromEntries(filteredBooks)) });
+      .json({ books: JSON.stringify(filteredBooks) });
   } catch (err) {
     return res.status(400).json({ mesaage: "issue in filter by title" });
   }
 });
 
 //  Get book review
-public_users.get("/review/:isbn", function (req, res) {
+public_users.get("/review/:isbn", async function (req, res) {
   try {
-    const reviews = books[req.params.isbn]?.reviews || {};
-    return res.status(200).json({ reviews: JSON.stringify(reviews) });
+    const book = await getBooks({ isbn: req.params.isbn })
+    return res.status(200).json({ reviews: JSON.stringify(book?.reviews || {}) });
   } catch (err) {
     return res.status(400).json({ mesaage: "issue in retrive reviews" });
   }
